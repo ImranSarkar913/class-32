ad08a8d26a779b718d1231a447ecbc6acdb1226b
unknown <ImranSarkar913>
2024-02-15 22:01:29 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+            primary: '#F40404'
+          },
+          maxWidth:{
+            container: '1144px'
+          },
+          fontFamily: {
+            'pops': ['Poppins', 'sans-serif'],
+          },
+          backgroundImage: {
+            'banner': "url('./image/banar.png')", 
+             'service' : "url('./image/service.png')",
+             'pic' : "url('./image/p5.png')",
+             'img' : "url('./image/p6.png')",
+             'com' : "url('./image/p7.png')",
+             'photo' : "url('./image/p8.png')",
+             'imgs' : "url('./image/p9.png')",
+             'pics' : "url('./image/p10.png')",
+          },
+          Width:{
+            contain: '1920'
+          },
+        
+        }
+      }
+    }
